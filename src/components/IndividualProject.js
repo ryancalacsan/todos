ef36f8c75c05bdb487841b62753585882cc88c3f
Ryan Calacsan <ryancalacsan@gmail.com>
2020-07-23 01:22:05 -0500

    modified delete modal

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -37,31 +37,28 @@ export const IndividualProject = ({ project }) => {
         aria-label="Confirm deletion of project"
       >
         <FaTrashAlt />
-        {showConfirm && (
-          <div className="project-delete-modal">
-            <div className="project-delete-modal__inner">
-              <p>Are you sure you want to delete this project?</p>
-              <button
-                type="button"
-                onClick={() => deleteProject(project.docId)}
-              >
-                Delete
-              </button>
-              <span
-                onClick={() => setShowConfirm(!showConfirm)}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') setShowConfirm(!showConfirm);
-                }}
-                tabIndex={0}
-                role="button"
-                aria-label="Cancel adding project, do not delete"
-              >
-                Cancel
-              </span>
-            </div>
-          </div>
-        )}
       </span>
+      {showConfirm && (
+        <div className="project-delete-modal">
+          <div className="project-delete-modal__inner">
+            <p>Delete "{project.name}" project?</p>
+            <button type="button" onClick={() => deleteProject(project.docId)}>
+              Delete
+            </button>
+            <span
+              onClick={() => setShowConfirm(!showConfirm)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') setShowConfirm(!showConfirm);
+              }}
+              tabIndex={0}
+              role="button"
+              aria-label="Cancel adding project, do not delete"
+            >
+              Cancel
+            </span>
+          </div>
+        </div>
+      )}
     </>
   );
 };
